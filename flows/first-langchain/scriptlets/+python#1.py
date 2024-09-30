1c17e7d9f38f37133d344d1cd65e058c77beaaa1
TaoZeyu <i@taozeyu.com>
2024-09-30 12:19:59 +0800

    feat: add RAG

---
+++ flows/first-langchain/scriptlets/+python#1.py
@@ -0,0 +1,7 @@
+from oocana import Context
+
+def main(inputs: dict, context: Context):
+  out = {
+    "question": "what is the story behind the millions-year war between sheeps and flowers? how does the little prince understand why flowers have thorns?"
+  }
+  return { "out": out }
