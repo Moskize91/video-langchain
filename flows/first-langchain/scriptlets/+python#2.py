3aee87e873e91c76862b91021ff8bca51b919442
TaoZeyu <i@taozeyu.com>
2024-09-30 13:36:28 +0800

    feat: structure output

--- flows/first-langchain/scriptlets/+python#2.py
+++ flows/first-langchain/scriptlets/+python#2.py
@@ -3,11 +3,16 @@ from oocana import Context
 from langchain_core.runnables import RunnablePassthrough
 
 def main(inputs: dict, context: Context):
+  source_id: int = 0
+
   def format_docs(docs):
+    nonlocal source_id
     texts: list[str] = []
     for doc in docs:
       content = re.sub(r"\s+", " ", doc.page_content)
-      texts.append(content)
+      texts.append(f"Source: {source_id}\n{content}")
+      source_id += 1
+
     return "\n\n".join(texts)
 
   retriever = inputs["retriever"]
---
+++ flows/first-langchain/scriptlets/+python#4.py
@@ -0,0 +1,27 @@
+from __future__ import annotations
+
+from oocana import Context
+from pydantic import Field, BaseModel
+
+class CitedAnswer(BaseModel):
+  """anwser the user question based only on the given sources."""
+
+  anser_sentences: list[AnswerSentence] = Field(
+    ...,
+  )
+
+class AnswerSentence(BaseModel):
+  sentence: str = Field(
+    ...,
+  )
+  citations: list[int] = Field(
+    ...,
+    description=" ".join([
+      "the interger IDs of the SPECIFIC sources with justify the sentences.",
+      "choose the most appropriate source and ignore sources that are irrelevant to the",
+      "sentence. if there are no sources, to be an empty list."
+    ])
+  )
+
+def main(inputs: dict, context: Context):
+  return { "output": CitedAnswer }
